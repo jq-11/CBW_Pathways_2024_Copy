35be7654da7a96b7465c8e3c940d9e181bd9c143
Ruth Isserlin <ruth.isserlin@utoronto.ca>
2021-05-05 13:56:48 -0400

    Updated module 2 lab
    updated to a newer baderlab geneset file, updated screen shots that were version dependent and general syntax and layout fixes

---
+++ modify_gmt_based_on_gs_size.R
@@ -0,0 +1,21 @@
+#install.packages("ActivePathways")
+
+filename <- "Human_GOBP_AllPathways_no_GO_iea_September_01_2020_symbol"
+
+#read in the gmt file
+gmt_file <- file.path(getwd(),"Module2","gprofiler",
+                      "data",
+                      paste(filename,".gmt",sep=""))
+
+genesets <- ActivePathways::read.GMT(gmt_file)
+
+geneset_lengths <- lapply(genesets,FUN = function(x){length(x$genes)})
+
+#restrict the genesets to length of 250
+genesets_length250 <- genesets[which(geneset_lengths <= 250)]
+
+output_gmt_file <- file.path(getwd(),"Module2","gprofiler",
+                      "data",
+                      paste(filename,"_maxgssize250.gmt",sep=""))
+
+ActivePathways::write.GMT(genesets_length250,output_gmt_file)
